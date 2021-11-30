c833dafaf9f6eebc668c3f67f4af0d02e8fdbe1d
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-11-30 16:07:42 -0500

    routing from new to root setup

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -3,6 +3,7 @@ import Layout from '../../components/Layout';
 import { Form, Button, Input, Message } from 'semantic-ui-react';
 import factory from '../../ethereum/factory';
 import web3 from '../../ethereum/web3';
+import { Router } from '../../routes';
 
 class CampaignNew extends Component {
     state = {
@@ -22,6 +23,8 @@ class CampaignNew extends Component {
             .send({ 
                 from: accounts[0] // don't need to send specified gas since metamask will calculate it automatically
             });
+
+            Router.pushRoute('/');
         } catch (err) {
             this.setState({ errorMessage: err.message });
         }
