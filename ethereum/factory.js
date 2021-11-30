cb6ab124ca3d411a18f0c6390508ffd87346cfeb
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-11-30 16:38:01 -0500

    updated factory address

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
     JSON.parse(CampaignFactory.interface),
-    '0xe3164111e28Fc82Dce64F7CEb535D68E666d39BB'
+    '0x18dEe2aD2C54f8F29EF148CC019f520D76a4421a'
 );
 
 export default instance;
