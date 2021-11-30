164407ce5e1107922f226d9304bfe6d1d138b900
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-11-30 09:38:47 -0500

    installed next, react and react dom

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -3,9 +3,9 @@ const Web3 = require('web3');
 const compiledFactory = require('./build/CampaignFactory.json');
 
 const provider = new HDWalletProvider(
-  'system woman actress obvious example surge birth spring harbor transfer width clog',
+  '',
   // remember to change this to your own phrase!
-  'https://rinkeby.infura.io/v3/d55c4f57b80445d284aa7754f355aaeb'
+  ''
   // remember to change this to your own endpoint!
 );
 const web3 = new Web3(provider);
