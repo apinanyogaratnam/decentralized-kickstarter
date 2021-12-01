c97f09443fce5bde96da1eb3e2729c2f58271b93
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-11-30 20:42:38 -0500

    showing make contribution section

---
+++ pages/campaigns/requests/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class RequestIndex extends Component {
+    render() {
+        return (
+            <div>
+                Reqyest 
+            </div>
+        );
+    }
+}
+
+export default RequestIndex;
